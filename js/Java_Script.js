e29a28de8e02114f6b3f8ade33e351467447d792
ColinLee1998 <colinlee12171998@gmail.com>
2022-01-11 21:59:06 +0800

    20220111 用陣列迴圈新增標籤(非常重要)

--- js/Java_Script.js
+++ js/Java_Script.js
@@ -1,4 +1,31 @@
-AddArticle();
+AddArticles();
+
+function AddArticles() {
+
+    // 主要的 article id=main
+    let Main = document.getElementById("main");
+
+    // 產生3個標籤
+    let ArtId = ['PortfolioBox', 'AboutBox', 'ProjectBox'];
+
+    for (let i = 0; i < ArtId.length; i++) {
+
+        // 新增 article
+        let Art = document.createElement('article');
+
+        // 新增 article 屬性
+        Art.setAttribute("id", ArtId[i]);
+
+        // 在 Main 裡面新增子元件 Art
+        Main.appendChild(Art);
+
+        // 呼叫聯絡表單
+        let connacttable = document.getElementById("ContactBox");
+        
+        // 放在聯絡表單之上
+        connacttable.before(Art);
+    }
+}
 
 function AddArticle() {
     // 呼叫主要的 article id=main
@@ -8,11 +35,30 @@ function AddArticle() {
     let art = document.createElement("article");
 
     // 新增 article 的屬性 setAttribute("id or class", "自訂名稱") id
-    art.setAttribute("id", "Portfolio");
+    art.setAttribute("id", "PortfolioBox");
 
     // innerHTML 可寫入純文字，還可以寫入 html 的標籤
-    art.innerHTML = "測試";
+    // art.innerHTML = "<h1>在 Por 元素的開頭</h1>";
+
+    // 在 article 新增 h1
+    let h1 = document.createElement('h1');
+
+    h1.textContent = "作品分享";
+
+    art.appendChild(h1);
 
     // 取得 html 標籤(元素)中添加的子元素
     Main.appendChild(art);
-}
+
+    // 1. 在被選取的元素結尾插入內容 append() 置入被選取元素之中
+
+    // 2. 在被選取的元素開頭插入內容 prepend() 置入被選取元素之中
+
+    // 3. 在被選取的元素之後插入內容 after() 置入被選取元素標籤之後
+
+    // 4. 在被選取的元素之前插入內容 before() 置入被選取元素標籤之前
+
+    let Por = document.getElementById("PortfolioBox");
+
+    Por.before(art);
+};
--- js/javascript.js
+++ js/javascript.js
@@ -227,3 +227,18 @@ function ArrayCode() {
         console.log(Item[i]);
     }
 }
+
+let Items =['Item01', 'Item02', 'Item03'];
+
+for (let i = 0; i < Items.length; i++) {
+
+    // 在 html 內新增標籤 li
+    let liName = document.createElement('li');
+
+    // 在陣列內的資料，置入 li 之內
+    liName.textContent = Item[i];
+
+    // 在哪個 html 標籤內，產生 li 清單物件
+    let Div = document.getElementById('ProjectContent');
+    Div.appendChild(liName);
+}

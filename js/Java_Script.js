13a935b5e86602b50157fbd3fe0a87ce2ca339be
ColinLee1998 <colinlee12171998@gmail.com>
2022-01-18 19:46:59 +0800

    20220118 新增 article 裡面的標籤

--- js/Java_Script.js
+++ js/Java_Script.js
@@ -2,28 +2,39 @@
 
 function AddArticles() {
 
+    // 陣列 id
+    let ArtId = ['PortfolioBox', 'AboutBox', 'ProjectBox'];
+    let ArtH1 = ['PortfolioTitle', 'AboutTitle', 'ProjectTitle'];
+    let ArtH1Con = ['平面作品', '網頁作品','影音作品'];
+    let ArtH1PorDiv = ['MGraphic', 'MWeb', 'MAudio'];
+
     // 主要的 article id=main
     let Main = document.getElementById("main");
-
-    // 產生3個標籤
-    let ArtId = ['PortfolioBox', 'AboutBox', 'ProjectBox'];
-
+    
+    // 產出3個標籤
     for (let i = 0; i < ArtId.length; i++) {
 
-        // 新增 article
+        // 新增 article 標籤
         let Art = document.createElement('article');
+        let H1 = document.createElement("h1");
 
-        // 新增 article 屬性
+        // 新增 article 的 id 與 h1 的 class
         Art.setAttribute("id", ArtId[i]);
+        H1.setAttribute("class", ArtH1[i]);
 
-        // 在 Main 裡面新增子元件 Art
+        // 在框架內新增子元素 main > article > h1
         Main.appendChild(Art);
+        Art.appendChild(H1);
+
 
         // 呼叫聯絡表單
         let connacttable = document.getElementById("ContactBox");
         
         // 放在聯絡表單之上
         connacttable.before(Art);
+
+        // 置入文字
+        H1.innerHTML = ArtH1Con[i];
     }
 }
 
@@ -64,6 +75,7 @@ function AddArticle() {
 
     // 取得 html 標籤(元素)中添加的子元素
     Main.appendChild(art);
+
 
     // 1. 在被選取的元素結尾插入內容 append() 置入被選取元素之中
 
